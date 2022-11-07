af0311ed1565a446375a1b782208a1f52a7b61c1
brunoracki <bruno.racki@fer.hr>
2022-11-07 17:56:09 +0100

    Added beta DTO , guys please check it out

---
+++ Clanstvo/Clanstvo.DataAccess.SqlServer/DTOs/ClanRangZaslugaDto.cs
@@ -0,0 +1,32 @@
+﻿using ClanstvoWebApi.Data.DbModels;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clanstvo.DataAccess.SqlServer.DTOs
+{
+    internal class ClanRangZaslugaDto
+    {
+        public partial class ClanRangZasluga
+        {
+            [Key]
+            public int ClanId { get; set; }
+            [Key]
+            public int RangZaslugaId { get; set; }
+            [Column(TypeName = "date")]
+            public DateTime Datum { get; set; }
+
+            [ForeignKey("ClanId")]
+            [InverseProperty("ClanRangZasluga")]
+            public virtual ClanoviDto Clan { get; set; }
+            [ForeignKey("RangZaslugaId")]
+            [InverseProperty("ClanRangZasluga")]
+            public virtual RangZaslugaDto RangZasluga { get; set; }
+        }
+    }
+}
---
+++ Clanstvo/Clanstvo.DataAccess.SqlServer/DTOs/RangZaslugaDto.cs
@@ -0,0 +1,32 @@
+﻿using ClanstvoWebApi.Data.DbModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clanstvo.DataAccess.SqlServer.DTOs
+{
+    internal class RangZaslugaDto
+    {
+        
+        public RangZaslugaDto()
+        {
+            ClanRangZasluga = new HashSet<ClanRangZaslugaDto>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Unicode(false)]
+        public string Naziv { get; set; }
+
+        [InverseProperty("RangZasluga")]
+        public virtual ICollection<ClanRangZaslugaDto> ClanRangZasluga { get; set; }
+    
+}
+}
